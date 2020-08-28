299f923dba33cefee543c54944c769f55fbb54a5
lakshay-dutt <lakshay91dutt@gmail.com>
2020-08-29 01:21:23 +0530

    initial deployment to heroku

---
+++ src/components/Forms/LoginForm/index.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+import "./style.scss";
+
+function LoginForm() {
+  return <Fragment></Fragment>;
+}
+export default LoginForm;
---
+++ src/components/Forms/SearchForm/index.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+import "./style.scss";
+
+function SearchForm() {
+  return <Fragment></Fragment>;
+}
+export default SearchForm;
